4d313a2b2354ea49a0027a20d9e63f9bd5d229be
Karthik G Raj <karthikgraj2004@gmail.com>
2026-07-05 17:33:28 +0530

    fix: remove vite-plugin-compression2 to resolve build-time peer dependency conflict

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteCompression from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    viteCompression({
-      algorithm: 'gzip',
-      ext: '.gz',
-    }),
-    viteCompression({
-      algorithm: 'brotliCompress',
-      ext: '.br',
-    })
+    react()
   ],
   build: {
     rollupOptions: {
